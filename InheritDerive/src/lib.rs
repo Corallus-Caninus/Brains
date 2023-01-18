e23a1d7f3f2184ffdd215f10a468d27b0291d71e
Corallus-Caninus <ward.joshua92@yahoo.com>
2023-01-18 05:15:13 -0800

    allow non inherited, extendible structs (but not generics yet)

--- InheritDerive/src/lib.rs
+++ InheritDerive/src/lib.rs
@@ -32,28 +32,91 @@ use syn::{parse_macro_input, DeriveInput};
 pub fn derive_inherit_state(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
     let name = &ast.ident;
-    let gen = quote! {
-        impl InheritState for #name {
-            fn get_mut_layer_state(&mut self) -> &mut LayerState {
-                &mut self.0
-            }
-            fn get_layer_state(&self) -> &LayerState {
-                &self.0
-            }
-        }
-        impl InitializeLayer for #name {
-            fn init() -> Self {
-                #name(LayerState {
-                    input: None,
-                    input_size: 0,
-                    output_size: 0,
-                    width: 0,
-                    activation: None,
-                    dtype: DataType::Float,
-                })
-            }
-        }
+    //allow non inheritence (if self.0 is self.layer_state)
+    let is_inherited = match ast.data {
+        syn::Data::Struct(ref data) => match data.fields {
+            syn::Fields::Named(ref fields) => fields
+                .named
+                .iter()
+                .any(|field| field.ident.as_ref().unwrap() == "0"),
+            _ => true,
+        },
+        _ => false,
     };
-
-    gen.into()
+    if is_inherited {
+        let gen = quote! {
+            impl InheritState for #name{
+                fn get_mut_layer_state(&mut self) -> &mut LayerState {
+                    &mut self.0
+                }
+                fn get_layer_state(&self) -> &LayerState {
+                    &self.0
+                }
+            }
+            impl InitializeLayer for #name {
+                fn init() -> Self {
+                    #name(LayerState {
+                        input: None,
+                        input_size: 0,
+                        output_size: 0,
+                        width: 0,
+                        activation: None,
+                        dtype: DataType::Float,
+                    })
+                }
+            }
+        };
+        gen.into()
+    } else {
+        //get all fields except self.layer_state in a list
+        let fields = match ast.data {
+            syn::Data::Struct(ref data) => match data.fields {
+                syn::Fields::Named(ref fields) => fields
+                    .named
+                    .iter()
+                    .filter(|field| field.ident.as_ref().unwrap() != "layer_state")
+                    .map(|field| field.ident.as_ref().unwrap())
+                    .collect::<Vec<_>>(),
+                _ => vec![],
+            },
+            _ => vec![],
+        };
+        let gen = quote! {
+            impl InheritState for #name{
+                fn get_mut_layer_state(&mut self) -> &mut LayerState {
+                    &mut self.layer_state
+                }
+                fn get_layer_state(&self) -> &LayerState {
+                    &self.layer_state
+                }
+            }
+            impl InitializeLayer for #name {
+                fn init() -> Self {
+                    #name {
+                        layer_state: LayerState {
+                            input: None,
+                            input_size: 0,
+                            output_size: 0,
+                            width: 0,
+                            activation: None,
+                            dtype: DataType::Float,
+                        }
+                        //also initialize any other fields as long as they have
+                        //default implementation
+                        #(, #fields: Default::default())*
+                    }
+                }
+            }
+        };
+        gen.into()
+        //let gen = quote! {
+        //        impl InheritState for #name {
+        //            fn get_mut_layer_state(&mut self) -> &mut LayerState {
+        //                &mut self.0
+        //            }
+        //            fn get_layer_state(&self) -> &LayerState {
+        //                &self.0
+        //            }
+        //}
+    }
 }
